086ea895a5d878398761b03c7b86d80002884bc2
Sukesh Reddy <117748776+Sukesh79@users.noreply.github.com>
2023-08-24 07:57:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let elem = document.createElement("p");
+let browserName = navigator.appName;
+let version = navigator.appVersion;
+elem.textContent = "You are using " + browserName + " version " + version;
+
+let parent = document.getElementById("browser-info");
+parent.appendChild(elem);
